d207a41213cd7e1abeabb3955c5ca38184374fd0
vasuskimo <vasusrinivasan@gmail.com>
2019-11-30 22:34:42 -0800

    removed unncessary things

--- src/PrivacySettings.js
+++ src/PrivacySettings.js
@@ -1,18 +1,3 @@
-/**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
 import $ from 'jquery';
@@ -20,18 +5,10 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import Router from './Router';
 
-/**
- * Handles the Privacy Settings UI.
- */
 export default class PrivacySettings {
-  /**
-   * Initializes the user's profile UI.
-   * @constructor
-   */
   constructor(firebaseHelper) {
     this.firebaseHelper = firebaseHelper;
 
-    // DOM Elements for Privacy Consent Modal
     this.privacyDialogButton = $('.privacy-dialog-link');
     this.privacyDialog = $('#privacy-dialog');
     this.privacyDialogSave = $('.privacy-save');
@@ -41,19 +18,11 @@ export default class PrivacySettings {
     this.uploadButton = $('button#add');
     this.mobileUploadButton = $('button#add-floating');
 
-    // Event bindings for Privacy Consent Dialog
     this.privacyDialogButton.click(() => this.showPrivacyDialog());
     this.privacyDialogSave.click(() => this.savePrivacySettings());
     this.allowDataProcessing.change(() => this.toggleSubmitStates());
-    // Prevent the escape key from dismissing the dialog
-    this.privacyDialog.keydown((e) => {
-      if (e.keyCode === 27) return false;
-    });
   }
 
-  /**
-   * Sets initial state of Privacy Dialog.
-   */
   showPrivacyDialog() {
     this.initializePrivacySettings();
     if (window.dialogPolyfill && !this.privacyDialog.get(0).showModal) {
@@ -62,10 +31,6 @@ export default class PrivacySettings {
     this.privacyDialog.get(0).showModal();
   }
 
-  /**
-   * Disable the submit button for the privacy settings until data privacy
-   * policy is agreed to.
-   */
   toggleSubmitStates() {
     if (this.allowDataProcessing.is(':checked')) {
       this.privacyDialogSave.removeAttr('disabled');
@@ -74,10 +39,6 @@ export default class PrivacySettings {
     }
   }
 
-  /**
-   * Fetches previously saved privacy settings if they exist and
-   * enables the Submit button if user has consented to data processing.
-   */
   async initializePrivacySettings() {
     const uid = firebase.auth().currentUser.uid;
     if (this.savedPrivacySettings === undefined) {
@@ -100,11 +61,7 @@ export default class PrivacySettings {
     }
   }
 
-  /**
-   * Saves new privacy settings and closes the privacy dialog.
-   */
   savePrivacySettings() {
-    // uid of signed in user
     const uid = firebase.auth().currentUser.uid;
     const settings = {
       data_processing: this.allowDataProcessing.prop('checked'),
