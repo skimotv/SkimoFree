a675c981ac6eccd10f55398e5e6af57bca8f4b62
vasuskimo <vasusrinivasan@gmail.com>
2019-11-30 22:48:37 -0800

    removed more files

--- src/async-loaded-components.js
+++ src/async-loaded-components.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import MessagingHelper from './MessagingHelper';
 import AuthData from './AuthData';
 import FirebaseHelper from './FirebaseHelper';
 import PrivacySettings from './PrivacySettings';
@@ -8,6 +7,5 @@ import SkimoPage from './SkimoPage';
 
 const firebaseHelper = new FirebaseHelper();
 const privacySettings = new PrivacySettings(firebaseHelper);
-const messagingHelper = new MessagingHelper(firebaseHelper);
 export const skimoPage = new SkimoPage();
 new AuthData(firebaseHelper, privacySettings);
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,7 +1,5 @@
 'use strict';
 
 import firebase from 'firebase/app';
-import 'firebase/messaging';
 import firebaseConfig from './firebase-config.json';
 firebase.initializeApp(firebaseConfig.result);
-firebase.messaging();
